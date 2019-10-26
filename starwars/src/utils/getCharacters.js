f65c8ce05d77f525f67df8c44397c19a9c3091ed
Josh Cirre <joshcirre@gmail.com>
2019-10-26 09:10:46 -0700

    set up scaffolding for project and used AXIOS request to pull data :rocket:

---
+++ starwars/src/utils/getCharacters.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export const get = axios.create({
+    responseType: 'json',
+    method: 'get'
+})
+
+// Implement our API pull.
+// We are looking for pages, setPages, currentPage, and setCharacters
